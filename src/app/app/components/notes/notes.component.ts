8809d2e3626b041ee87f2ca68a46737be476f184
Sebastian Golian <sebastiangolian@gmail.com>
2019-09-11 20:32:04 +0200

    add expansion-panel

--- src/app/app/components/notes/notes.component.ts
+++ src/app/app/components/notes/notes.component.ts
@@ -15,15 +15,6 @@ export class NotesComponent implements OnInit {
   editNote = false;
   editNoteForm: FormGroup;
 
-  dataSource: any = [
-    { name: 'Yogurt', calories: 159, fat: 6, carbs: 24, protein: 4 },
-    { name: 'Sandwich', calories: 237, fat: 9, carbs: 37, protein: 4 },
-    { name: 'Eclairs', calories: 262, fat: 16, carbs: 24, protein: 6 },
-    { name: 'Cupcakes', calories: 305, fat: 4, carbs: 67, protein: 4 },
-    { name: 'Gingerbreads', calories: 356, fat: 16, carbs: 49, protein: 4 },
-  ];
-  displayedColumns: string[] = ['name', 'calories', 'fat', 'carbs', 'protein']
-
 
   constructor(private formBuilder: FormBuilder, private notesModel: NotesService) { }
 
