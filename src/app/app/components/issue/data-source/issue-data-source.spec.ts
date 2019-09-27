a806a54117e186355b456769bcc507df988d4ce0
Sebastian Golian <sebastiangolian@gmail.com>
2019-09-27 12:00:39 +0200

    remove isssue-data-source test

--- src/app/app/components/issue/data-source/issue-data-source.spec.ts
+++
@@ -1,7 +0,0 @@
-import { IssueDataSource } from './issue-data-source';
-
-describe('IssueDataSource', () => {
-  it('should create an instance', () => {
-    expect(new IssueDataSource()).toBeTruthy();
-  });
-});
