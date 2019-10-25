ffa3f424c9a2e07bbc46dcfcadfc33c1d9ac8e8e
Sebastian Golian <sebastiangolian@gmail.com>
2019-10-25 13:51:20 +0200

    commit add component

--- src/app/app/app.module.ts
+++ src/app/app/app.module.ts
@@ -14,6 +14,7 @@ import { IssueComponent } from './components/issue/issue.component';
 import { IssueAddComponent } from './components/issue/dialogs/issue-add/issue-add.component';
 import { IssueEditComponent } from './components/issue/dialogs/issue-edit/issue-edit.component';
 import { IssueDeleteComponent } from './components/issue/dialogs/issue-delete/issue-delete.component';
+import { CommitComponent } from './components/commit/commit.component';
 
 
 @NgModule({
@@ -26,7 +27,8 @@ import { IssueDeleteComponent } from './components/issue/dialogs/issue-delete/is
     IssueComponent,
     IssueAddComponent,
     IssueEditComponent,
-    IssueDeleteComponent
+    IssueDeleteComponent,
+    CommitComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/app/components/commit/commit.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './commit.component.html',
+  styleUrls: ['./commit.component.css']
+})
+export class CommitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
